c786a0b2e0771a05022192445ba92f18137fb55c
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-06-05 19:34:20 +0200

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 const { createApp } = Vue
 
-    createApp({
-        data() {
+createApp({
+    data() {
         return {
-            
+        emails: []
         }
     },
-    methods: {
+    
+    methods:{
+        emailGenerator: function(){
+            for (let i = 0; i < 10; i++){    
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        email = response.data.response;
+                        this.emails.push(email)
+                    });
+            }
+        }
+    },
 
+    created() {
+        this.emailGenerator();
     }
-    }).mount('#app')
+}).mount('#app')
